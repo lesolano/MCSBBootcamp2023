ef43a66b302ec6478761d0a05c5e7c3ee5fb760a
lesolano <72961771+lesolano@users.noreply.github.com>
2023-08-30 16:23:19 -0700

    precashfilespartC

---
+++ Module2/ProjectDiscreteLogistic_LS.m
@@ -0,0 +1,31 @@
+% How much caffeine is there in the jar?
+
+% n - number of days
+% x - fraction of caffeinated 
+
+r= 0.1 %r
+K =0.6 %K
+
+%(1,:) = [0 0.1 0.3 0.5 0.7];%initial conditions
+
+%y = x(n) + r(1-x(n)/K)x(n)
+nMax = 60; % max number of days to simulate
+
+x = zeros(1,nMax); % number of rabbits
+x(1) = 0.2; % initial condition
+
+for n=2:nMax
+    
+    x(n) = x(n-1) + r*(1-x(n-1)/K)*x(n-1)
+  
+    
+end % finished loop through days
+
+% THE MODEL ^
+% ------------------------------------------
+% THE BEHAVIOR / THE OUTPUT ? 
+
+figure(1); 
+plot(x,'-ok');
+ylabel('fraction caffeinated')
+xlabel('Days')
